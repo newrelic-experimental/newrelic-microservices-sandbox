b2a0c58734f677479d92b1e7fccebd9e5eb7b881
Eric Mittelhammer <code@ericmittelhammer.com>
2022-03-12 22:02:39 +0000

    proxy working

--- apps/gateway/server.js
+++ apps/gateway/server.js
@@ -1,6 +1,5 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
-const proxy = require('koa-proxies')
 const pino = require('pino');
 const logger = require('koa-pino-logger')({
   logger: pino(),
@@ -15,36 +14,25 @@ const logger = require('koa-pino-logger')({
     },
   }
 });
-const constructURL = require("construct-url");
+
+const proxy = require('./middleware/proxy');
 
 const superheroes_protocol = process.env.SUPERHEROES_PROTOCOL || "http"
 const superheroes_host = process.env.SUPERHEROES_HOST || "superheroes"
 
 const app = new Koa();
 const router = new Router();
-const api = new Router();
-
-api.get(
-  '/superheroes(.*)',
-  proxy("/", {
-    target: `${superheroes_protocol}://${superheroes_host}/`,    
-    rewrite: (path, ctx) => `${ctx.params[0] || ""}?${ctx.querystring}`,
-    logs: (ctx, target) => {
-      ctx.log.info('%s %s proxy to -> %s', ctx.req.method, ctx.req.oldPath, new URL(ctx.req.url, target))
-    },
-    events: {
-      error (err, req, res) { console.log("yp") }
-    }
-  })
-);
-
-router.use('/api', api.routes());
 
 router.get('/ping', (ctx, next) => {
   ctx.body = {
     message: "healthy"
   }
 });
+
+
+router.all('/api/(.*)', proxy({
+  '/api/superheroes(.*)': `${superheroes_protocol}://${superheroes_host}/v1/superheroes$1`
+}));
 
 app
   .use(logger)
