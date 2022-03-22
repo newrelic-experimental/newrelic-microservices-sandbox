7d04ed0c9d1d9aeca87234dd5b51e6a52e2a56c2
Eric Mittelhammer <code@ericmittelhammer.com>
2022-03-22 01:59:18 +0000

    adding random and slug routes

--- apps/superheroes/apis/v1/superheroes.py
+++ apps/superheroes/apis/v1/superheroes.py
@@ -1,6 +1,7 @@
 import logging
+import random
 
-from flask_restx import Namespace, Resource, errors, fields
+from flask_restx import Namespace, Resource, errors, fields, reqparse
 
 from repository import Superheroes, db
 
@@ -73,6 +74,18 @@ superhero_model = api.model(
 #         '''List all cats'''
 #         return CATS
 
+@api.route("/random")
+class RandomSuperheroes(Resource):
+    @api.doc("")
+    @api.marshal_list_with(superhero_model)
+    def get(self):
+        """Fetch a random number of superheroes"""
+        parser = reqparse.RequestParser()
+        parser.add_argument('num', type=int, default=1, help='Invalid num')
+        args = parser.parse_args()
+        logging.info(f"fetching {args['num']} random superheroes")
+        superheroes = random.sample(db.all(), args["num"])
+        return superheroes
 
 @api.route("/<id>")
 @api.param("id", "The superhero's identifier")
@@ -86,5 +99,20 @@ class Superhero(Resource):
         sh = db.get(Superheroes.id == int(id))
         if sh is None:
             errors.abort(404, message="superhero not found", id=id)
+        else:
+            return sh
+
+@api.route("/slug/<slug>")
+@api.param("slug", "The superhero's slug")
+@api.response(404, "Superhero not found")
+class SuperheroBySlig(Resource):
+    @api.doc("get_superhero_by_slug")
+    @api.marshal_with(superhero_model)
+    def get(self, slug):
+        """Fetch a superhero given its slug"""
+        logging.info(f"fetching superhero with slug: {slug}")
+        sh = db.get(Superheroes.slug == slug)
+        if sh is None:
+            errors.abort(404, message="superhero not found", slug=slug)
         else:
             return sh
