a5756d6a04e097d8d80e7c125b9322f104953262
Eric Mittelhammer <code@ericmittelhammer.com>
2022-06-02 18:23:02 -0400

    explicit remote ref

--- scripts/create-demo-commits.sh
+++ scripts/create-demo-commits.sh
@@ -23,7 +23,7 @@ export GIT_COMMITTER_DATE=$COMMIT_1_DATE
 COMMIT_1_HASH=$(git commit-tree -m "Working hard on v1 of our API!" $COMMIT_1_TREE)
 git checkout $COMMIT_1_HASH
 
-git restore --staged --source=main apps
+git restore --staged --source=origin/main apps
 
 export GIT_AUTHOR_NAME=$COMMIT_2_NAME
 export GIT_AUTHOR_EMAIL=$COMMIT_2_EMAIL
