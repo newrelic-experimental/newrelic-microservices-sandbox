b3e71f02fa5bcc97a728ad5f717a3f44b8fc854c
Eric Mittelhammer <code@ericmittelhammer.com>
2022-06-06 16:34:35 -0400

    adding workflow to demo branch

--- scripts/create-demo-commits.sh
+++ scripts/create-demo-commits.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-git fetch origin main
+REF=${SOURCE_REF:-"main"}
+TARGET=${INPUT_BRANCHNAME:-"demo"}
+git update-ref -d refs/heads/$TARGET
+
+git fetch origin $REF
 
 COMMIT_1_DATE=$(date --date="7 day ago" -u +"%Y-%m-%dT%H:%M:%SZ")
 COMMIT_1_NAME=${INPUT_USER1_NAME:-"User One"}
@@ -12,16 +16,17 @@ COMMIT_2_DATE=$(date --date="3 day ago" -u +"%Y-%m-%dT%H:%M:%SZ")
 COMMIT_2_NAME=${INPUT_USER2_NAME:-"User Two"}
 COMMIT_2_EMAIL=${INPUT_USER2_EMAIL:-"usertwo@example.com"}
 
-COMMIT_1_TREE=$(git ls-tree backstage apps | git mktree)
-
 export GIT_AUTHOR_NAME=$COMMIT_1_NAME
 export GIT_AUTHOR_EMAIL=$COMMIT_1_EMAIL
 export GIT_AUTHOR_DATE=$COMMIT_1_DATE
 export GIT_COMMITTER_NAME=$COMMIT_1_NAME
 export GIT_COMMITTER_EMAIL=$COMMIT_1_EMAIL
 export GIT_COMMITTER_DATE=$COMMIT_1_DATE
-COMMIT_1_HASH=$(git commit-tree -m "Working hard on v1 of our API!" $COMMIT_1_TREE)
-git checkout $COMMIT_1_HASH
+git checkout -f --orphan $TARGET
+git restore --staged --source=origin/$REF .github/workflows/build-push-image.yml
+git restore --staged --source=origin/backstage apps
+git commit -m "Working hard on v1 of our API!"
+
 
 git restore --staged --source=origin/main apps
 
@@ -32,5 +37,3 @@ export GIT_COMMITTER_NAME=$COMMIT_2_NAME
 export GIT_COMMITTER_EMAIL=$COMMIT_2_EMAIL
 export GIT_COMMITTER_DATE=$COMMIT_2_DATE
 git commit -m "Updating API to version 2!"
-
-git update-ref -m "moving branch to updated commit" refs/heads/${INPUT_BRANCHNAME:-"demo"} HEAD
